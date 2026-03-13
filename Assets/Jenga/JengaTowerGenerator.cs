36d6ac1d057734ca148d6566466ee3745aa497d3
Leonel Merino <leonelmerino@gmail.com>
2026-03-13 11:05:28 -0300

    Tower created okay. However, blocks have to much friction.

--- Assets/Jenga/JengaTowerGenerator.cs
+++ Assets/Jenga/JengaTowerGenerator.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class JengaTowerGenerator : MonoBehaviour
@@ -8,66 +7,38 @@ public class JengaTowerGenerator : MonoBehaviour
     public GameObject blockPrefab;
     public Material[] blockMaterials;
 
-    [Header("Real block dimensions")]
+    [Header("Block Dimensions")]
     public float blockLength = 0.075f;
     public float blockHeight = 0.015f;
     public float blockWidth = 0.025f;
 
-    [Header("Tower settings")]
+    [Header("Tower Settings")]
     public int levels = 6;
     public float horizontalSpacing = 0.0005f;
     public float verticalSpacing = 0.0002f;
-    public bool generateOnStart = true;
+    public float dropHeight = 0.01f;
 
-    private readonly List<Rigidbody> spawnedBodies = new List<Rigidbody>();
+    [Header("Physics")]
+    public float settleVelocity = 0.01f;
+    public float settleAngularVelocity = 0.01f;
+    public float maxSettleTime = 1.0f;
+
+    private bool isBuilding = false;
 
     void Start()
     {
-        if (generateOnStart)
-            StartCoroutine(GenerateTowerStable());
+        StartCoroutine(BuildTower());
     }
 
-    [ContextMenu("Reset Tower")]
-    public void ResetTower()
+    IEnumerator BuildTower()
     {
-        if (Application.isPlaying)
-            StartCoroutine(ResetTowerCoroutine());
-    }
-
-    [ContextMenu("Clear Tower")]
-    public void ClearTower()
-    {
-        if (Application.isPlaying)
-            ClearTowerPlayMode();
-    }
-
-    private IEnumerator ResetTowerCoroutine()
-    {
-        ClearTowerPlayMode();
-        yield return null;
-        yield return new WaitForFixedUpdate();
-        yield return StartCoroutine(GenerateTowerStable());
-    }
-
-    private void ClearTowerPlayMode()
-    {
-        for (int i = transform.childCount - 1; i >= 0; i--)
-        {
-            Destroy(transform.GetChild(i).gameObject);
-        }
-    }
-
-    private IEnumerator GenerateTowerStable()
-    {
-        spawnedBodies.Clear();
+        isBuilding = true;
 
         for (int level = 0; level < levels; level++)
         {
             bool rotate = (level % 2 == 1);
 
             float y = level * (blockHeight + verticalSpacing) + (blockHeight * 0.5f);
-
-            List<Rigidbody> levelBodies = new List<Rigidbody>();
 
             for (int i = -1; i <= 1; i++)
             {
@@ -78,52 +49,88 @@ public class JengaTowerGenerator : MonoBehaviour
 
                 if (!rotate)
                 {
-                    // Bloques largos en X, separados en Z
                     localPos = new Vector3(0f, y, lateralOffset);
                     rot = Quaternion.identity;
                 }
                 else
                 {
-                    // Bloques largos en Z, separados en X
                     localPos = new Vector3(lateralOffset, y, 0f);
                     rot = Quaternion.Euler(0f, 90f, 0f);
                 }
 
+                Vector3 spawnPos = transform.position + localPos + Vector3.up * dropHeight;
+
                 GameObject block = Instantiate(
                     blockPrefab,
-                    transform.position + localPos,
+                    spawnPos,
                     rot,
                     transform
                 );
 
-                // Asignar color al hijo visual
                 Renderer r = block.GetComponentInChildren<Renderer>();
-                if (r != null && blockMaterials != null && blockMaterials.Length > 0)
+                if (r != null && blockMaterials.Length > 0)
                 {
                     r.material = blockMaterials[Random.Range(0, blockMaterials.Length)];
                 }
 
                 Rigidbody rb = block.GetComponent<Rigidbody>();
+
                 if (rb != null)
                 {
-                    rb.isKinematic = true;
                     rb.velocity = Vector3.zero;
                     rb.angularVelocity = Vector3.zero;
-                    levelBodies.Add(rb);
-                    spawnedBodies.Add(rb);
+                    rb.isKinematic = false;
+                    rb.useGravity = true;
+
+                    yield return StartCoroutine(WaitUntilSettled(rb));
                 }
             }
+        }
 
-            yield return null;
-            yield return new WaitForFixedUpdate();
+        isBuilding = false;
+    }
 
-            foreach (Rigidbody rb in levelBodies)
+    IEnumerator WaitUntilSettled(Rigidbody rb)
+    {
+        float timer = 0f;
+
+        while (timer < maxSettleTime)
+        {
+            if (rb == null)
+                yield break;
+
+            if (rb.velocity.magnitude < settleVelocity &&
+                rb.angularVelocity.magnitude < settleAngularVelocity)
             {
-                if (rb != null)
-                    rb.isKinematic = false;
+                yield break;
             }
 
+            timer += Time.fixedDeltaTime;
             yield return new WaitForFixedUpdate();
+        }
+    }
+
+    [ContextMenu("Reset Tower")]
+    public void ResetTower()
+    {
+        if (!Application.isPlaying || isBuilding) return;
+
+        StartCoroutine(ResetCoroutine());
+    }
+
+    IEnumerator ResetCoroutine()
+    {
+        ClearTower();
+        yield return new WaitForFixedUpdate();
+        yield return StartCoroutine(BuildTower());
+    }
+
+    [ContextMenu("Clear Tower")]
+    public void ClearTower()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            Destroy(transform.GetChild(i).gameObject);
         }
     }
 }
